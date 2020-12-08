9b27cee89ad701d1c4c31bb7b41bf8fe7eb1c214
Samuel Gomez <samuel.gomez@kaleidos.net>
2020-12-08 18:03:14 +0100

    Pantalla de inicio

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={styles.title}>Libre</Text>
+      <Text style={styles.title}>Kwiz</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Aquí el PIN'
+        // onChangeText={text => onChangeText(text)}
+        // value={value}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Aquí tu Alias'
+        // onChangeText={text => onChangeText(text)}
+        // value={value}
+      />
+      <Button
+        // onPress={onPressLearnMore}
+        height= '54'
+        style={styles.boton}
+        title="¡VAMOS!"
+        color="#000"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
@@ -14,8 +35,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#83ffcd',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    paddingHorizontal: 20
   },
+  input: {
+    height: 54,
+    width:'100%',
+    backgroundColor:'#fff',
+    color:'#000',
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 20,
+    marginBottom: 16,
+    fontSize: 18,
+  },
+  title: {
+    fontWeight: "700",
+    lineHeight: 56,
+    fontSize: 56,
+    textAlign: "center",
+    width:'100%',
+  }
 });
