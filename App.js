83765afabb89457daecef095bca6f98d1bccf712
Samuel Gomez <samuel.gomez@kaleidos.net>
2020-12-09 09:14:28 +0100

    Add Logo

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Text style={styles.title}>Libre</Text>
-      <Text style={styles.title}>Kwiz</Text>
+      <View style={styles.header}>
+        <Image style={styles.logo} source={require('./assets/front_src_imgs_kwizzie.png')}/>
+        <Text style={styles.title}>Libre</Text>
+        <Text style={styles.title}>Kwiz</Text>
+      </View>
       <TextInput
         style={styles.input}
         placeholder='Aquí el PIN'
@@ -20,14 +23,15 @@ export default function App() {
         // onChangeText={text => onChangeText(text)}
         // value={value}
       />
-      <Button
-        // onPress={onPressLearnMore}
-        height= '54'
-        style={styles.boton}
-        title="¡VAMOS!"
-        color="#000"
-        accessibilityLabel="Learn more about this purple button"
-      />
+      <View style={styles.button}>
+        <Button
+          // onPress={onPressLearnMore}
+          height= '54'
+          title="¡VAMOS!"
+          color="#000"
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
     </View>
   );
 }
@@ -51,11 +55,18 @@ const styles = StyleSheet.create({
     marginBottom: 16,
     fontSize: 18,
   },
+  header: {
+    alignItems: "center",
+    textAlign: "center",
+    width:'100%',
+    marginBottom: 32,
+  },
+  logo: {
+    marginBottom: 16
+  },
   title: {
     fontWeight: "700",
     lineHeight: 56,
     fontSize: 56,
-    textAlign: "center",
-    width:'100%',
   }
 });
