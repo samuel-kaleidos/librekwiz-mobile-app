75a11cc61cd38aefe94421f7e2f4cea3ed0b7ff8
Samuel Gomez <samuel.gomez@kaleidos.net>
2020-12-10 17:28:11 +0100

    Add navigation

--- App.js
+++ App.js
@@ -1,88 +1,23 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput, Image, TouchableOpacity } from 'react-native';
+import JoinGame from './screens/JoinGame'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 // ws://librekwiz.com/ws/quiz/4H74/Lucas%20Corbray
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View style={styles.header}>
-        <Image style={styles.logo} source={require('./assets/front_src_imgs_kwizzie.png')}/>
-        <Text style={styles.title}>Libre</Text>
-        <Text style={styles.title}>Kwiz</Text>
-      </View>
-      <TextInput
-        style={styles.input}
-        placeholder='Aquí el PIN'
-        // onChangeText={text => onChangeText(text)}
-        // value={value}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder='Aquí tu Alias'
-        // onChangeText={text => onChangeText(text)}
-        // value={value}
-      />
-      <TouchableOpacity
-        accessible={true}
-        accessibilityHint="Entrar al Qwiz"
-        accessibilityLabel="¡Vamos!"
-        accessibilityRole="button"
-        onPress={()=>console.log('Pressed')}>
-        <View style={styles.button}>
-          <Text style={styles.btnText}>¡VAMOS!</Text>
-        </View>
-      </TouchableOpacity>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Join Game"
+          component={JoinGame}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#83ffcd',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-    paddingHorizontal: 20
-  },
-  input: {
-    height: 54,
-    width:'100%',
-    backgroundColor:'#fff',
-    color:'#000',
-    borderColor: 'gray',
-    borderWidth: 1,
-    paddingHorizontal: 20,
-    marginBottom: 16,
-    fontSize: 18,
-    fontWeight:"700"
-  },
-  header: {
-    alignItems: "center",
-    textAlign: "center",
-    width:'100%',
-    marginBottom: 32,
-  },
-  logo: {
-    marginBottom: 16
-  },
-  title: {
-    fontWeight: "700",
-    lineHeight: 56,
-    fontSize: 56,
-  },
-  button: {
-    backgroundColor: "#000",
-    borderRadius: 5,
-    height: 54,
-    justifyContent: "center"
-  },
-  btnText: {
-    color: "#fff",
-    fontSize: 18,
-    fontWeight:"700",
-    textAlign: "center"
-  }
-});
---
+++ screens/JoinGame.js
@@ -0,0 +1,88 @@
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
+
+// ws://librekwiz.com/ws/quiz/4H74/Lucas%20Corbray
+
+const JoinGame = () => 
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Image style={styles.logo} source={require('../assets/front_src_imgs_kwizzie.png')}/>
+        <Text style={styles.title}>Libre</Text>
+        <Text style={styles.title}>Kwiz</Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder='Aquí el PIN'
+        // onChangeText={text => onChangeText(text)}
+        // value={value}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Aquí tu Alias'
+        // onChangeText={text => onChangeText(text)}
+        // value={value}
+      />
+      <TouchableOpacity
+        accessible={true}
+        accessibilityHint="Entrar al Qwiz"
+        accessibilityLabel="¡Vamos!"
+        accessibilityRole="button"
+        onPress={()=>console.log('Pressed')}>
+        <View style={styles.button}>
+          <Text style={styles.btnText}>¡VAMOS!</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#83ffcd',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    paddingHorizontal: 20
+  },
+  input: {
+    height: 54,
+    width:'100%',
+    backgroundColor:'#fff',
+    color:'#000',
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 20,
+    marginBottom: 16,
+    fontSize: 18,
+    fontWeight:"700"
+  },
+  header: {
+    alignItems: "center",
+    textAlign: "center",
+    width:'100%',
+    marginBottom: 32,
+  },
+  logo: {
+    marginBottom: 16
+  },
+  title: {
+    fontWeight: "700",
+    lineHeight: 56,
+    fontSize: 56,
+  },
+  button: {
+    backgroundColor: "#000",
+    borderRadius: 5,
+    height: 54,
+    justifyContent: "center"
+  },
+  btnText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight:"700",
+    textAlign: "center"
+  }
+});
+
+export default JoinGame;
